10a3172112dfd4d7564e99afb2617f20e2207afb
Andrew Dzobko <mytyber@gmail.com>
2023-06-14 21:16:28 -0700

    Reformated services section to simple columns

--- src/components/FloatingBullets.jsx
+++ src/components/FloatingBullets.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import "../styles/services.css"
+import "../colors.css"
 import { useInView } from "react-intersection-observer";
 
 export default function FloatingBullets (props) {
@@ -27,165 +28,89 @@ export default function FloatingBullets (props) {
 
     return (
         <section ref={ref} className={visible ? "animate" : ""} id="five-bullet-points">
-            <div className="bullet-column">
-                <div id="point-1" onClick={expandPoint} className={`floating-bullet ${"point-1" === expandedPoint ? "active" : ""}`}>
-                    <div className="wrapper">
-                        <div><span className="material-symbols-outlined">hardware</span></div>
-                        <div><h3>Wyburzenia, Rozbiórki, Demontaże</h3></div>
-                        <div>
-                            <ul>
-                                <li>Zabezpieczenie</li>
-                                <li>Demontaż całej łazienki</li>
-                                <li>Skucie starej posadzki</li>
-                                <li>Wyburzenie ścian działowych</li>
-                                <li>Demontaż mebli</li>
-                                <li>Demontaż glazury terakoty</li>
-                                <li>Zerwanie starej farby</li>
-                                <li>Zerwanie tynków</li>
-                                <li>Poszerzenie otworów w ścianach</li>
-                                <li>Demontaż deski podłogowej</li>
-                                <li>Demontaż paneli</li>
-                                <li>Demontaż sufitu podwieszanego</li>
-                                <li>Demontaż drzwi</li>
-                                <li>Demontaż futryn</li>
-                                <li>Demontaż okien</li>
-                                <li>Demontaż płyt GK ze ścian</li>
-                                <li>Demontaż karniszy</li>
-                                <li>Demontaż lamp</li>
-                                <li>Zerwanie tapet</li>
-                                <li>Wynos gruzu</li>
-                            </ul>
-                        </div>
-                    </div>
-                </div>
-                <div id="point-2" onClick={expandPoint} className={`floating-bullet ${"point-2" === expandedPoint ? "active" : ""}`}>
-                    <div className="wrapper">
-                        <div><span className="material-symbols-outlined">imagesearch_roller</span></div>
-                        <div><h3>Prace Malarskie</h3></div>
-                        <div>
-                            <ul>
-                                <li>Szlifowanie ścian przed rozpoczęciem prac malarskich</li>
-                                <li>Naprawa pęknięć</li>
-                                <li>Wzmocnienie ścian siatką z tynkiem</li>
-                                <li>Naprawa bruzd po elektryce/hydraulice</li>
-                                <li>Gruntowanie</li>
-                                <li>Wyrównanie ścian-sufitu tynkiem</li>
-                                <li>Tynkowanie ścian po listwach prowadzących</li>
-                                <li>Gładzie</li>
-                                <li>Gładź dekoracyjna</li>
-                                <li>Akryl</li>
-                            </ul>
-                        </div>
+            <div id="point-1" onClick={expandPoint} className={`floating-bullet ${"point-1" === expandedPoint ? "active" : ""}`}>
+                <div className="wrapper">
+                    <div><span className="material-symbols-outlined">hardware</span></div>
+                    <div><h3>Biały montaż</h3></div>
+                    <div>
+                        <ul>
+                        <li>Montaż mebli łazienkowych, wanien, brodzików, kabin natryskowych i paneli prysznicowych, wc, geberitów.</li>
+                        <li>Zakładanie umywalek, postumentów, zlewozmywaków, baterii, kraników i zaworów.</li>
+                        </ul>
                     </div>
                 </div>
             </div>
-            <div className="bullet-column">
-                <div id="point-3" onClick={expandPoint} className={`floating-bullet ${"point-3" === expandedPoint ? "active" : ""}`}>
-                    <div className="wrapper">
-                        <div><span className="material-symbols-outlined">construction</span></div>
-                        <div><h3>Budowa / Przebudowa - Praca W Pomieszczeniach</h3></div>
-                        <div>
-                            <ul>
-                                <li>Murowanie ścian</li>
-                                <li>Przedścianki z profili i płyt GK (pojedyncze lub podwójne płytowanie)</li>
-                                <li>Sufit podwieszany z profili i płyt GK (pojedyncze lub podwójne płytowanie)</li>
-                                <li>Montaż płyt GK na klej</li>
-                                <li>Sufit powieszany (styl artystyczny)</li>
-                                <li>Układanie paneli</li>
-                                <li>Wzmocnienie ścian</li>
-                                <li>Montaż cokołów</li>
-                                <li>Wzmocnienie nadproża</li>
-                                <li>Montaż listew maskujących</li>
-                                <li>Montaż listew przysufitowych</li>
-                                <li>Montaż drzwi + ościeżnicy</li>
-                                <li>Montaż karniszy</li>
-                                <li>Montaż oświetlenia o różnych wymiarach</li>
-                                <li>Montaż parapetów</li>
-                                <li>Montaż mocowania dla telewizora</li>
-                                <li>Montaż telewizora</li>
-                                <li>Montaż elementów do zajęć sportowych</li>
-                                <li>Wbudowanie peszla pod telewizor w ścianę</li>
-                                <li>Przyklejenie tapety</li>
-                            </ul>
-                        </div>
-                    </div>
-                </div>
-                <img id="brush-illustration" alt="golden paintbrush" src="./ai_illustrations/brush.png"/>
-                <div id="point-4" onClick={expandPoint} className={`floating-bullet ${"point-4" === expandedPoint ? "active" : ""}`}>
-                    <div className="wrapper">
-                        <div><span className="material-symbols-outlined">bolt</span></div>
-                        <div><h3>Elektryka</h3></div>
-                        <div>
-                            <ul>
-                                <li>Nowe punkty elektryczne</li>
-                                <li>Doprowadzenie kabli</li>
-                                <li>Tworzenie puszek rozdzielczych</li>
-                                <li>Podłączenie skrzynki elektrycznej</li>
-                                <li>Podłączenie + montaż oświetlenia LED</li>
-                                <li>Doprowadzenie kabli RTV</li>
-                                <li>Podłączenie domofonu</li>
-                                <li>Podłączenie TV</li>
-                                <li>Podłączenie klimatyzacji</li>
-                                <li>Podłączenie sprzętu AGD</li>
-                                <li>Podłączenie żaluzji elektrycznych</li>
-                            </ul>
-                        </div>
+            <div id="point-2" onClick={expandPoint} className={`floating-bullet ${"point-2" === expandedPoint ? "active" : ""}`}>
+                <div className="wrapper">
+                    <div><span className="material-symbols-outlined">imagesearch_roller</span></div>
+                    <div><h3>Ściany i sufity</h3></div>
+                    <div>
+                        <ul>
+                            <li>Lmalowanie</li>
+                            <li>Tynki</li>
+                            <li>Gładzie</li>
+                            <li>Sufity podwieszane</li>
+                            <li>Ścianki działowe</li>
+                        </ul>
                     </div>
                 </div>
             </div>
-            <div className="bullet-column">
-                <div id="point-5" onClick={expandPoint} className={`floating-bullet ${"point-5" === expandedPoint ? "active" : ""}`}>
-                    <div className="wrapper">
-                        <div><span className="material-symbols-outlined">bathtub</span></div>
-                        <div><h3>Łazienka, kuchnia</h3></div>
-                        <div>
-                            <ul>
-                                <li>Wylewka betonowa</li>
-                                <li>Wylewka samopoziomująca</li>
-                                <li>Podłoga z keramzytu i płyty farmacell</li>
-                                <li>Montaż + podłączenie ogrzewania podłogowego (rodzaj: elektryczne)</li>
-                                <li>Hydroizolacja</li>
-                                <li>Układanie płytek o standardowych wymiarach, wieloformatowych</li>
-                                <li>Zrobienie półek z płytek</li>
-                                <li>Cięcie płytek pod kątem 45</li>
-                                <li>Wywiercanie otworów w płytce</li>
-                                <li>Montaż/zabudowa stelażu WC / BD</li>
-                                <li>Zabudowa rur (pionowych - poziomych)</li>
-                                <li>Zabudowa rur z rewizją pod liczniki wody</li>
-                                <li>Zabudowa wanny</li>
-                                <li>Zabudowa rur od wentylacji</li>
-                                <li>Zabudowa pod półki</li>
-                                <li>Zabudowa wanny z wnęką na stopy</li>
-                                <li>Montaż sufitu podwieszanego</li>
-                                <li>Wygłuszenie ścian .</li>
-                                <li>Montaż zlewu, umywalki, kranu, grzejnika</li>
-                                <li>Montaż zestawu natynkowego</li>
-                                <li>Montaż kabiny prysznicowej, wanny</li>
-                                <li>Montaż gniazdek, włączników, oświetlenia</li>
-                                <li>Montaż/składanie szafek</li>
-                                <li>Montaż + docięcie blatu</li>
-                                <li>Podłączenie piekarnika, płyty indukcyjnej, okapu, zmywarki, pralki</li>
-                                <li>Gładzie</li>
-                                <li>Malowanie</li>
-                            </ul>
-                        </div>
+            <div id="point-3" onClick={expandPoint} className={`floating-bullet ${"point-3" === expandedPoint ? "active" : ""}`}>
+                <div className="wrapper">
+                    <div><span className="material-symbols-outlined">grid_view</span></div>
+                    <div><h3>Usługi glazurnicze</h3></div>
+                    <div>
+                        <ul>
+                        <li>Układanie glazury, mozaiki, terakoty, gresu, granitogresu oraz każdego rodzaju kafli zgodnie z projektem klienta.</li>
+                        <li>Szlifowanie płytek pod kątem 45°</li>
+                        <li>Docinanie cokołów oraz wycinanie otworów.</li>
+                        </ul>
                     </div>
                 </div>
-                <div id="point-6" onClick={expandPoint} className={`floating-bullet ${"point-6" === expandedPoint ? "active" : ""}`}>
-                    <div className="wrapper">
-                        <div><span className="material-symbols-outlined">plumbing</span></div>
-                        <div><h3>Hydraulika</h3></div>
-                        <div>
-                            <ul>
-                                <li>Zmiana układu hydraulicznego w łazience</li>
-                                <li>Zmiana układu hydraulicznego w kuchni</li>
-                                <li>Zmiana podejścia pod grzejnik</li>
-                                <li>Nowe punkty zimnej/ciepłej wody</li>
-                                <li>Montaż stelażu WC</li>
-                                <li>Montaż stelażu BD</li>
-                                <li>Prace hydrauliczne po zalaniu</li>
-                            </ul>
-                        </div>
+            </div>
+            {/* <img id="brush-illustration" alt="golden paintbrush" src="./ai_illustrations/brush.png"/> */}
+            <div id="point-4" onClick={expandPoint} className={`floating-bullet ${"point-4" === expandedPoint ? "active" : ""}`}>
+                <div className="wrapper">
+                    <div><span className="material-symbols-outlined">bolt</span></div>
+                    <div><h3>Elektryka</h3></div>
+                    <div>
+                        <ul>
+                        <li>Kucie pod instalacje</li>
+                        <li>Łączenie ze skrzynkami bezpiecznikowymi</li>
+                        <li>Osadzanie puszek</li>
+                        <li>Montaż gniazdek i wyłączników</li>
+                        <li>Montaż oświetlenia (lamp, kinkietów, halogenów)</li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+            <div id="point-5" onClick={expandPoint} className={`floating-bullet ${"point-5" === expandedPoint ? "active" : ""}`}>
+                <div className="wrapper">
+                    <div><span className="material-symbols-outlined">nest_wifi_gale</span></div>
+                    <div><h3>Podłogi</h3></div>
+                    <div>
+                        <ul>
+                        <li>Układanie paneli</li>
+                        <li>Parkietu</li>
+                        <li>Deski podłogowej oraz deski barlineckiej</li>
+                        <li>Montaż listew przypodłogowych</li>
+                        <li>Progów</li>
+                        <li>Parapetów.</li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+            <div id="point-6" onClick={expandPoint} className={`floating-bullet ${"point-6" === expandedPoint ? "active" : ""}`}>
+                <div className="wrapper">
+                    <div><span className="material-symbols-outlined">plumbing</span></div>
+                    <div><h3>Hydraulika</h3></div>
+                    <div>
+                        <ul>
+                            <li>Zmiana układu hydraulicznego</li>
+                            <li>Nowe punkty zimnej/ciepłej wody</li>
+                            <li>Prace hydrauliczne po zalaniu</li>
+                            <li>Montaż stelażu BD oraz WC.</li>
+                        </ul>
                     </div>
                 </div>
             </div>
